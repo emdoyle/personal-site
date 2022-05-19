85c3f7f506ee2e7ef3cc2fddbfbe184bd4febcb9
Evan Doyle <evanmdoyle@gmail.com>
2022-05-19 10:10:13 -0700

    Add layout styling and placeholders for main sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,35 +21,71 @@ export default function Home() {
       <Header />
       <Content>
         <Section>
-          <div className="flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
-            <div className="w-32 sm:w-52 rounded border border-smoke shadow-[0px_0px_8px_10px_rgba(112,112,112,.25)]">
-              <Image
-                alt="Evan Doyle Headshot"
-                className="rounded"
-                layout="responsive"
-                src={Headshot}
-              />
-            </div>
-            <div className="w-full sm:w-1/2">
-              <h1 className="text-2xl sm:text-4xl pb-4">Hi, I&apos;m Evan!</h1>
-              <p className="sm:text-2xl pb-4">
-                I&apos;m a <strong>Full-Stack</strong> Web Developer from the SF
-                Bay Area with Django and React expertise.
-              </p>
-              <div className="flex justify-between items-center sm:text-2xl">
-                <a>
-                  <span className="underline">Hire me</span> &gt;
-                </a>
-                <a>
-                  <span className="underline">View services</span> &gt;
-                </a>
+          <div className="flex flex-col justify-center items-center h-full">
+            <div className="flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
+              <div className="w-32 sm:w-52 rounded border border-smoke shadow-[0px_0px_8px_10px_rgba(112,112,112,.25)]">
+                <Image
+                  alt="Evan Doyle Headshot"
+                  className="rounded"
+                  layout="responsive"
+                  src={Headshot}
+                />
+              </div>
+              <div className="w-full sm:w-1/2 lg:w-2/3">
+                <h1 className="text-2xl sm:text-4xl lg:text-5xl pb-4">
+                  Hi, I&apos;m Evan!
+                </h1>
+                <p className="sm:text-2xl lg:text-3xl pb-4">
+                  I&apos;m a <strong>Full-Stack</strong> Web Developer from the
+                  SF Bay Area with Django and React expertise.
+                </p>
+                <div className="flex justify-start items-center gap-8 sm:text-xl lg:text-2xl">
+                  <a href="#" className="hover:text-smoke-400">
+                    <span className="underline">Hire me</span> &gt;
+                  </a>
+                  <a href="#" className="hover:text-smoke-400">
+                    <span className="underline">View services</span> &gt;
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </Section>
-        <Section />
-        <Section />
-        <Section />
+        <Section>
+          <div className="flex flex-col justify-start items-center h-full">
+            <div className="flex justify-start items-center h-24 w-5/6 sm:max-w-[1100px]">
+              <h2 className="text-2xl sm:text-4xl font-semibold">
+                Featured Work
+              </h2>
+            </div>
+            <div className="h-full flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
+              <p>Left</p>
+              <p>Right</p>
+            </div>
+          </div>
+        </Section>
+        <Section>
+          <div className="flex flex-col justify-start items-center h-full">
+            <div className="flex justify-end items-center h-24 w-5/6 sm:max-w-[1100px]">
+              <h2 className="text-2xl sm:text-4xl font-semibold">Services</h2>
+            </div>
+            <div className="h-full flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
+              <p>Left</p>
+              <p>Right</p>
+            </div>
+          </div>
+        </Section>
+        <Section>
+          <div className="flex flex-col justify-start items-center h-full">
+            <div className="flex justify-center items-center h-24 w-5/6 sm:max-w-[1100px]">
+              <h2 className="text-2xl sm:text-4xl font-semibold">Hire Me</h2>
+            </div>
+            <div className="h-full flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
+              <p>Left</p>
+              <p>Right</p>
+            </div>
+          </div>
+        </Section>
       </Content>
       <Footer />
     </Container>
