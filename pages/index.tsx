273d5fcff5e64b998388a920e492037574e9a1c2
Evan Doyle <evanmdoyle@gmail.com>
2022-05-21 16:10:31 -0700

    Service block placeholders and responsiveness, fix link overflow on small screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,23 +23,23 @@ export default function Home() {
         <Section>
           <div className="flex flex-col justify-center items-center h-full">
             <div className="flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
-              <div className="w-32 sm:w-52 rounded border border-smoke shadow-[0px_0px_8px_10px_rgba(112,112,112,.25)]">
+              <div className="w-32 sm:w-52 rounded-lg border border-smoke shadow-[0px_0px_8px_10px_rgba(112,112,112,.25)]">
                 <Image
                   alt="Evan Doyle Headshot"
-                  className="rounded"
+                  className="rounded-lg"
                   layout="responsive"
                   src={Headshot}
                 />
               </div>
               <div className="w-full sm:w-1/2 lg:w-2/3">
-                <h1 className="text-2xl sm:text-4xl lg:text-5xl pb-4">
+                <h1 className="text-3xl sm:text-4xl lg:text-5xl pb-4">
                   Hi, I&apos;m Evan!
                 </h1>
                 <p className="sm:text-2xl lg:text-3xl pb-4">
                   I&apos;m a <strong>Full-Stack</strong> Web Developer from the
                   SF Bay Area with Django and React expertise.
                 </p>
-                <div className="flex justify-start items-center gap-8 sm:text-xl lg:text-2xl">
+                <div className="flex justify-start items-center gap-8 sm:text-lg md:text-xl lg:text-2xl">
                   <a href="#" className="hover:text-ash">
                     <span className="underline">Hire me</span> &gt;
                   </a>
@@ -54,7 +54,7 @@ export default function Home() {
         <Section>
           <div className="flex flex-col justify-start items-center h-full">
             <div className="flex justify-start items-center h-24 w-5/6 sm:max-w-[1100px]">
-              <h2 className="text-2xl sm:text-4xl font-semibold">
+              <h2 className="text-3xl sm:text-4xl font-semibold">
                 Featured Work
               </h2>
             </div>
@@ -64,20 +64,23 @@ export default function Home() {
             </div>
           </div>
         </Section>
-        <Section height="h-[800px] lg:h-[450px]">
+        <Section height="h-[1000px] lg:h-[450px]">
           <div className="flex flex-col justify-start items-center h-full">
             <div className="flex justify-end items-center h-24 w-5/6 sm:max-w-[1100px] pb-8">
-              <h2 className="text-2xl sm:text-4xl font-semibold">Services</h2>
+              <h2 className="text-3xl sm:text-4xl font-semibold">Services</h2>
             </div>
-            <div className="h-full flex flex-col lg:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[1200px]">
-              <div className="flex flex-col justify-center items-center gap-4 rounded-lg bg-offwhite border border-smoke p-4 sm:p-8 shadow-[0px_0px_2px_6px_rgba(176,178,184,.25)] h-48 lg:h-64 w-2/3 lg:w-64">
-                <p>Hello, world</p>
+            <div className="h-full flex flex-col lg:flex-row gap-4 lg:gap-12 justify-between items-center w-5/6 sm:max-w-[1200px]">
+              <div className="flex flex-col justify-center items-center gap-4 rounded-lg bg-offwhite border border-smoke p-8 shadow-[0px_0px_2px_6px_rgba(176,178,184,.25)] h-64 w-2/3 lg:w-1/3">
+                <div className="w-60 h-full border-2 border-smoke rounded" />
+                <h1 className="text-xl lg:text-2xl">Service Title</h1>
               </div>
-              <div className="flex flex-col justify-center items-center gap-4 rounded-lg bg-offwhite border border-smoke p-4 sm:p-8 shadow-[0px_0px_2px_6px_rgba(176,178,184,.25)] h-48 lg:h-64 w-2/3 lg:w-64">
-                <p>Hello World</p>
+              <div className="flex flex-col justify-center items-center gap-4 rounded-lg bg-offwhite border border-smoke p-8 shadow-[0px_0px_2px_6px_rgba(176,178,184,.25)] h-64 w-2/3 lg:w-1/3">
+                <div className="w-60 h-full border-2 border-smoke rounded" />
+                <h1 className="text-xl lg:text-2xl">Service Title</h1>
               </div>
-              <div className="flex flex-col justify-center items-center gap-4 rounded-lg bg-offwhite border border-smoke p-4 sm:p-8 shadow-[0px_0px_2px_6px_rgba(176,178,184,.25)] h-48 lg:h-64 w-2/3 lg:w-64">
-                <p>Hello World</p>
+              <div className="flex flex-col justify-center items-center gap-4 rounded-lg bg-offwhite border border-smoke p-8 shadow-[0px_0px_2px_6px_rgba(176,178,184,.25)] h-64 w-2/3 lg:w-1/3">
+                <div className="w-60 h-full border-2 border-smoke rounded" />
+                <h1 className="text-xl lg:text-2xl">Service Title</h1>
               </div>
             </div>
           </div>
@@ -85,7 +88,7 @@ export default function Home() {
         <Section>
           <div className="flex flex-col justify-start items-center h-full">
             <div className="flex justify-center items-center h-24 w-5/6 sm:max-w-[1100px]">
-              <h2 className="text-2xl sm:text-4xl font-semibold">Hire Me</h2>
+              <h2 className="text-3xl sm:text-4xl font-semibold">Hire Me</h2>
             </div>
             <div className="h-full flex flex-col sm:flex-row gap-4 justify-between items-center w-5/6 sm:max-w-[900px]">
               <p>Left</p>
