3d632502cc3e1500c598d4a835acc3758fde6eb6
Evan Doyle <evanmdoyle@gmail.com>
2022-06-01 11:50:31 -0700

    offwhite text in header/footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC<{ className?: string }> = ({
 }) => {
   return (
     <footer
-      className={`h-16 sm:h-24 w-full flex flex-row gap-4 justify-center items-center px-4 sm:px-8 bg-smoke text-white ${className}`}
+      className={`h-16 sm:h-24 w-full flex flex-row gap-4 justify-center items-center px-4 sm:px-8 bg-smoke text-offwhite ${className}`}
     >
       <a
         className="text-xl border-r-2 border-white pr-4 py-1 hover:underline hover:text-ash"
--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ export const Header: React.FC<Props> = ({
 
   return (
     <>
-      <header className="h-16 sm:h-24 w-full flex flex-row justify-between items-center px-4 sm:px-8 bg-smoke text-white">
+      <header className="h-16 sm:h-24 w-full flex flex-row justify-between items-center px-4 sm:px-8 bg-smoke text-offwhite">
         <p className="text-2xl sm:text-4xl">Evan Doyle</p>
         <div className="flex flex-row justify-center items-center gap-2">
           {wideScreen ? (
