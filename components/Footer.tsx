97b2bacf0fe9547db525f80db059b0fa7b874076
Evan Doyle <evanmdoyle@gmail.com>
2022-06-01 11:39:35 -0700

    Add content to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { Github } from "../icons/Github";
+import { Twitter } from "../icons/Twitter";
+import { LinkedIn } from "../icons/LinkedIn";
+import { BlogIcon } from "../icons/BlogIcon";
 
 export const Footer: React.FC<{ className?: string }> = ({
   className = "",
 }) => {
   return (
     <footer
-      className={`h-16 sm:h-24 w-full flex flex-row justify-center items-center px-4 sm:px-8 bg-smoke text-white ${className}`}
+      className={`h-16 sm:h-24 w-full flex flex-row gap-4 justify-center items-center px-4 sm:px-8 bg-smoke text-white ${className}`}
     >
-      <p className="text-xl sm:text-2xl">Footer TODO</p>
+      <a
+        className="text-xl border-r-2 border-white pr-4 py-1 hover:underline hover:text-ash"
+        href="#"
+      >
+        Back to Top ⬆
+      </a>
+      <div className="pl-2">
+        <BlogIcon />
+      </div>
+      <Github />
+      <Twitter />
+      <LinkedIn />
     </footer>
   );
 };
